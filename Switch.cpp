9508b1448e9cad677048571780bd8e86d2ff3d02
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-10-27 11:38:57 +0500

    First commit

---
+++ Switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int age = 21;
+
+    switch(age){
+        case 16:
+            cout << "Fly!" << endl;
+            break;
+        case 18:
+            cout << "Fuck!" << endl;
+            break;
+        case 21:
+            cout << "Die!" << endl;
+            break;
+        default:
+            cout << "Not my piece of Cake!" << endl;
+
+        }
+}
